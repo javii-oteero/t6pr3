fed95965a0cf37565cf751f3f2021e315ce98fbd
javi <javii.oteero@gmail.com>
2024-03-12 13:21:01 +0100

    Initial commit con el primer método hecho

---
+++ src/es/iesmz/tests/Cuenta.java
@@ -0,0 +1,38 @@
+package es.iesmz.tests;
+
+import java.math.BigInteger;
+
+public class Cuenta {
+    private String numCuenta;
+    private String titular;
+
+    public Cuenta(String numCuenta, String titular) {
+        this.numCuenta = numCuenta;
+        this.titular = titular;
+    }
+
+    public boolean compruebaIBAN(String iban) {
+        boolean esValido = true;
+        if (iban.matches("ES\\d{32}")){
+            String cs = iban.substring(13) + "142800";
+            BigInteger num = new BigInteger(cs);
+            BigInteger restoDivision = num.mod(new BigInteger("97"));
+            BigInteger result = new BigInteger("98").subtract(restoDivision);
+            int digitoControl = result.intValue();
+            String digito;
+            if (digitoControl > 10){
+                digito = String.valueOf(digitoControl);
+            } else {
+                digito = "0" + digitoControl;
+            }
+            esValido = digito.equals(iban.substring(1, 3));
+        } else {
+            esValido = false;
+        }
+        return esValido;
+    }
+
+    public String generalIBAN(String entidad, String oficina, String dc, String cuenta) {
+        return "";
+    }
+}
